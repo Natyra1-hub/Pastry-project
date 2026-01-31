032186d5bfa23a2a0845d27894cc2c577afbf6b6
Natyra1-hub <nl73165@ubt-uni.net>
2026-01-31 09:50:22 +0100

    databasa

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+        class Database {
+            private $host='localhost';
+
+            private $dbname='projektiWEB';
+
+            private $username= 'root';
+
+            private $password='';
+
+            private $conn;
+
+        public function __construct(){
+            try{
+                 $this->conn= new PDO (dsn: "mysql:host={$this->host};dbname={$this->dbname}", username: $this->username, password: $this -> password);
+                 $this->conn->setAttribute(attribute: PDO::ATTR_ERRMODE, value : PDO ::ERRMODE_EXCEPTION );
+
+            }catch(PDOException $e){
+                die("Connection failed:" . $e->getMessage());
+
+                    }
+        }
+
+            public function  getConnection(): PDO{
+                return $this->conn;
+            }
+     }
+
+?>
